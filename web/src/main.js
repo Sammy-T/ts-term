0b72f4352005853b2dbbaa5f0ffd4f8da3df42e3
Sammy-T <cyam9019@gmail.com>
2025-07-31 22:27:27 -0400

    Respond to input

--- web/src/main.js
+++ web/src/main.js
@@ -8,4 +8,16 @@ term.loadAddon(fitAddon);
 term.open(document.querySelector('#xterm-container'));
 fitAddon.fit();
 
+window.addEventListener('resize', () => {
+	fitAddon.fit();
+});
+
 term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m \r\n$ ');
+
+term.onKey(({key, domEvent}) => {
+	term.write(key);
+
+	if(key === '\r') {
+		term.write('\n$ ');
+	}
+});

94a9df9787137bafa9299a853cc285d38e1cd3e6
Sammy-T <cyam9019@gmail.com>
2025-07-31 21:51:37 -0400

    Display xterm

--- web/src/main.js
+++ web/src/main.js
@@ -1,24 +1,11 @@
-import './style.css';
-import javascriptLogo from './javascript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.js';
+import { Terminal } from '@xterm/xterm';
+import { FitAddon } from '@xterm/addon-fit';
 
-document.querySelector('#app').innerHTML = `
-<div>
-	<a href="https://vite.dev" target="_blank">
-		<img src="${viteLogo}" class="logo" alt="Vite logo" />
-	</a>
-	<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-		<img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-	</a>
-	<h1>Hello Vite!</h1>
-	<div class="card">
-		<button id="counter" type="button"></button>
-	</div>
-	<p class="read-the-docs">
-		Click on the Vite logo to learn more
-	</p>
-</div>
-`;
+const term = new Terminal();
+const fitAddon = new FitAddon();
 
-setupCounter(document.querySelector('#counter'));
+term.loadAddon(fitAddon);
+term.open(document.querySelector('#xterm-container'));
+fitAddon.fit();
+
+term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m \r\n$ ');
